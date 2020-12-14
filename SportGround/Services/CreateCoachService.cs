790bd3d3235053710e5329e905e59f93a5cbc046
P0linux <pollykr2000@gmail.com>
2020-12-14 18:02:23 +0200

    Add CreateCoachService

---
+++ SportGround/Services/CreateCoachService.cs
@@ -0,0 +1,25 @@
+﻿using SportGround.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportGround.Services
+{
+    class CreateCoachService
+    {
+        Sport_ground_DBContext context;
+        public CreateCoachService(Sport_ground_DBContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateCoach(Coach coach, string sectionName)
+        {
+            var section = context.SportSections.Where(s => s.Name == sectionName).FirstOrDefault();
+            coach.SportSectionId = section.Id;
+            context.Coaches.Add(coach);
+            context.SaveChanges();
+        }
+    }
+}
--- SportGroundUi/Models/CoachParameters.cs
+++ SportGroundUi/Models/CoachParameters.cs
@@ -5,12 +5,15 @@ using System.Threading.Tasks;
 
 namespace SportGroundUI.Models
 {
-    public class VisitorParameters
+    public class CoachParameters
     {
         public string FirstName { get; set; }
         public string SecondName { get; set; }
-        public string PhoneNumber { get; set; }
         public decimal Age { get; set; }
         public string Sex { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Salary { get; set; }
+        public decimal Experience { get; set; }
+        public string SectionName { get; set; }
     }
 }
